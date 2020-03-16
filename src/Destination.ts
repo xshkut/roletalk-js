347edbc925e4e01daea3bc852b4953f714cf9a4e
xshkut <shkut.ap@gmail.com>
2020-03-16 13:05:03 +0200

    add Destination.peer() and Role.peer() methods

--- src/Destination.ts
+++ src/Destination.ts
@@ -1,193 +1,237 @@
-import { SetWithRoundRobin } from './misc/SetWithRoundRobin';
-import { Unit } from './Unit';
-import { EventEmitter } from 'events';
-import { MessageHeaders, EmitOptions, Context, sendableData } from './interfaces';
-import { Readable, Writable, ReadableOptions, WritableOptions } from 'stream';
-import { Peer } from './Peer';
+import { SetWithRoundRobin } from "./misc/SetWithRoundRobin";
+import { Unit } from "./Unit";
+import { EventEmitter } from "events";
+import {
+  MessageHeaders,
+  EmitOptions,
+  Context,
+  sendableData
+} from "./interfaces";
+import { Readable, Writable, ReadableOptions, WritableOptions } from "stream";
+import { Peer } from "./Peer";
 
-/**Destination represents a role (a service) of remote peers ([[Unit]]s). Destination is used as a gateway for outgoing communication. It implements round-robin load balancing between units 
- * 
+/**Destination represents a role (a service) of remote peers ([[Unit]]s). Destination is used as a gateway for outgoing communication. It implements round-robin load balancing between units
+ *
  * This class should not be instantiated directly. It is exposed for type declaration and documentation
-*/
+ */
 export class Destination extends EventEmitter {
-    private _name: string;
-    private _ready: boolean = false;
-    private _peer: Peer;
-    /**@internal */
-    _set: SetWithRoundRobin<Unit>;
-    /**@internal */
-    constructor(name: string, peer: Peer) {
-        super();
-        this._name = name;
-        this._peer = peer;
-        this._set = new SetWithRoundRobin();
-    }
+  private _name: string;
+  private _ready: boolean = false;
+  private _peer: Peer;
+  /**@internal */
+  _set: SetWithRoundRobin<Unit>;
+  /**@internal */
+  constructor(name: string, peer: Peer) {
+    super();
+    this._name = name;
+    this._peer = peer;
+    this._set = new SetWithRoundRobin();
+  }
 
-    /**Array of connected [[Unit]]s */
-    get units() {
-        return Array.from(this._set.values());
-    }
+  /**Array of connected [[Unit]]s */
+  get units() {
+    return Array.from(this._set.values());
+  }
 
-    /**Name of the destination */
-    get name() { return this._name }
+  /**Name of the destination */
+  get name() {
+    return this._name;
+  }
 
-    /**Send one-way message. Use cb argument to ensure the message has benn written to underlying socket. Returns [[Unit]] chosen to send data to */
-    send(event: string | EmitOptions, data: sendableData, cb?: (err: Error) => {}) {
-        let headers: MessageHeaders;
-        let unit: Unit | undefined;
-        if (typeof event === 'object' && event) {
-            headers = event;
-            if (event.unit) {
-                unit = this._set.has(this._peer._units.get(event.unit.id)!) ? event.unit : undefined;
-            } else {
-                unit = this._set.nextValue();
-            }
-        } else {
-            headers = { event }
-            unit = this._set.nextValue();
-        }
-        headers.role = this.name;
-        if (unit) {
-            unit.send(headers, data, cb);
-            return unit;
-        } else {
-            return undefined;
-        }
-    }
+  /**Get [[Peer]] instance*/
+  get peer() {
+    return this._peer;
+  }
 
-    /**Send one-way message to all units serving the role*/
-    broadcast(event: string | EmitOptions, data: sendableData) {
-        let headers = createMessageHeaders.call(this, event);
-        this._set.forEach((unit) => {
-            unit.send(headers, data);
-        });
+  /**Send one-way message. Use cb argument to ensure the message has benn written to underlying socket. Returns [[Unit]] chosen to send data to */
+  send(
+    event: string | EmitOptions,
+    data: sendableData,
+    cb?: (err: Error) => {}
+  ) {
+    let headers: MessageHeaders;
+    let unit: Unit | undefined;
+    if (typeof event === "object" && event) {
+      headers = event;
+      if (event.unit) {
+        unit = this._set.has(this._peer._units.get(event.unit.id)!)
+          ? event.unit
+          : undefined;
+      } else {
+        unit = this._set.nextValue();
+      }
+    } else {
+      headers = { event };
+      unit = this._set.nextValue();
     }
+    headers.role = this.name;
+    if (unit) {
+      unit.send(headers, data, cb);
+      return unit;
+    } else {
+      return undefined;
+    }
+  }
 
-    /**Send request. If cb argument is not provided, returns Promise<[[Context]]>
-     * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
-    */
-    request(event: string | EmitOptions, data: sendableData, cb?: (err: Error | null, ctx: Context) => void): Promise<Context> {
-        let headers: MessageHeaders;
-        let unit: Unit | undefined;
-        if (typeof event === 'object' && event) {
-            headers = event;
-            if (event.unit) {
-                unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!) ? event.unit : undefined;
-            } else {
-                unit = this._set.nextValue();
-            }
-        } else {
-            headers = { event }
-            unit = this._set.nextValue();
-        }
-        headers.role = this.name;
-        if (!unit) throw new Error(`No connected units with role [${this.name}]`);
-        return unit.request(headers, data, cb);
-    }
+  /**Send one-way message to all units serving the role*/
+  broadcast(event: string | EmitOptions, data: sendableData) {
+    let headers = createMessageHeaders.call(this, event);
+    this._set.forEach(unit => {
+      unit.send(headers, data);
+    });
+  }
 
-    /**Send requests to each unit of the role. Callback cb will be applied to each request 
-     * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
-    */
-    survey(event: string | EmitOptions, data: sendableData, cb: (err: Error | null, ctx: Context) => void) {
-        let headers = createMessageHeaders.call(this, event);
-        if (this._set.size < 1) new Error(`No connected units with role [${this.name}]`);
-        this._set.forEach(unit => unit.request(headers, data, cb));
-        return this._set.size;
+  /**Send request. If cb argument is not provided, returns Promise<[[Context]]>
+   * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
+   */
+  request(
+    event: string | EmitOptions,
+    data: sendableData,
+    cb?: (err: Error | null, ctx: Context) => void
+  ): Promise<Context> {
+    let headers: MessageHeaders;
+    let unit: Unit | undefined;
+    if (typeof event === "object" && event) {
+      headers = event;
+      if (event.unit) {
+        unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!)
+          ? event.unit
+          : undefined;
+      } else {
+        unit = this._set.nextValue();
+      }
+    } else {
+      headers = { event };
+      unit = this._set.nextValue();
     }
+    headers.role = this.name;
+    if (!unit) throw new Error(`No connected units with role [${this.name}]`);
+    return unit.request(headers, data, cb);
+  }
 
-    /**Send request to establish binary stream session. This end of the stream is readable (receives data only)
-     * If chosen [[Unit]] will reject the request, [[Writable]] will emit "error" event
-     * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
-    */
-    Readable(event: string | EmitOptions, data: sendableData, options?: ReadableOptions): Readable {
-        let headers: MessageHeaders;
-        let unit: Unit | undefined;
-        if (typeof event === 'object' && event) {
-            headers = event;
-            if (event.unit) {
-                unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!) ? event.unit : undefined;
-            } else {
-                unit = this._set.nextValue();
-            }
-        } else {
-            headers = { event }
-            unit = this._set.nextValue();
-        }
-        headers.role = this.name;
-        if (!unit) throw new Error(`No connected units with role [${this.name}]`);
-        return unit.Readable(headers, data, options);
-    }
+  /**Send requests to each unit of the role. Callback cb will be applied to each request
+   * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
+   */
+  survey(
+    event: string | EmitOptions,
+    data: sendableData,
+    cb: (err: Error | null, ctx: Context) => void
+  ) {
+    let headers = createMessageHeaders.call(this, event);
+    if (this._set.size < 1)
+      new Error(`No connected units with role [${this.name}]`);
+    this._set.forEach(unit => unit.request(headers, data, cb));
+    return this._set.size;
+  }
 
-    /**Send request to establish binary stream session. This end of the stream is writable (forwards data only)
-     * If chosen [[Unit]] will reject the request, [[Writable]] will emit "error" event
-     * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
-    */
-    Writable(event: string | EmitOptions, data: sendableData, options?: WritableOptions): Writable {
-        let headers: MessageHeaders;
-        let unit: Unit | undefined;
-        if (typeof event === 'object' && event) {
-            headers = event;
-            if (event.unit) {
-                unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!) ? event.unit : undefined;
-            } else {
-                unit = this._set.nextValue();
-            }
-        } else {
-            headers = { event }
-            unit = this._set.nextValue();
-        }
-        headers.role = this.name;
-        if (!unit) throw new Error(`No connected units with role [${this.name}]`);
-        return unit.Writable(headers, data, options);
+  /**Send request to establish binary stream session. This end of the stream is readable (receives data only)
+   * If chosen [[Unit]] will reject the request, [[Writable]] will emit "error" event
+   * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
+   */
+  Readable(
+    event: string | EmitOptions,
+    data: sendableData,
+    options?: ReadableOptions
+  ): Readable {
+    let headers: MessageHeaders;
+    let unit: Unit | undefined;
+    if (typeof event === "object" && event) {
+      headers = event;
+      if (event.unit) {
+        unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!)
+          ? event.unit
+          : undefined;
+      } else {
+        unit = this._set.nextValue();
+      }
+    } else {
+      headers = { event };
+      unit = this._set.nextValue();
     }
+    headers.role = this.name;
+    if (!unit) throw new Error(`No connected units with role [${this.name}]`);
+    return unit.Readable(headers, data, options);
+  }
 
-    /**@internal */
-    _addUnit(unit: Unit) {
-        if (this._set.has(unit)) return;
-        this._set.add(unit);
-        let wasReady = this.ready;
-        this._ready = true;
-        if (!wasReady) {
-            this.emit('open');
-        }
-        this.emit('unit', unit);
+  /**Send request to establish binary stream session. This end of the stream is writable (forwards data only)
+   * If chosen [[Unit]] will reject the request, [[Writable]] will emit "error" event
+   * If there are not connected [[Unit]]s serving corresponding [[Role]], method will throw synchronous error. To ensure at least one unit is connected, use [[Destination.ready]]
+   */
+  Writable(
+    event: string | EmitOptions,
+    data: sendableData,
+    options?: WritableOptions
+  ): Writable {
+    let headers: MessageHeaders;
+    let unit: Unit | undefined;
+    if (typeof event === "object" && event) {
+      headers = event;
+      if (event.unit) {
+        unit = this._peer.units.includes(this._peer._units.get(event.unit.id)!)
+          ? event.unit
+          : undefined;
+      } else {
+        unit = this._set.nextValue();
+      }
+    } else {
+      headers = { event };
+      unit = this._set.nextValue();
     }
+    headers.role = this.name;
+    if (!unit) throw new Error(`No connected units with role [${this.name}]`);
+    return unit.Writable(headers, data, options);
+  }
 
-    /**@internal */
-    _deleteUnit(unit: Unit) {
-        if (!this._set.has(unit)) return;
-        this._set.delete(unit);
-        if (this._set.size < 1) {
-            this._ready = false;
-            this.emit('close');
-        }
+  /**@internal */
+  _addUnit(unit: Unit) {
+    if (this._set.has(unit)) return;
+    this._set.add(unit);
+    let wasReady = this.ready;
+    this._ready = true;
+    if (!wasReady) {
+      this.emit("open");
     }
+    this.emit("unit", unit);
+  }
 
-    /**Returns true if there is at least one [[Unit]] serving corresponding [[Role]] */
-    get ready() {
-        return this._ready;
+  /**@internal */
+  _deleteUnit(unit: Unit) {
+    if (!this._set.has(unit)) return;
+    this._set.delete(unit);
+    if (this._set.size < 1) {
+      this._ready = false;
+      this.emit("close");
     }
+  }
+
+  /**Returns true if there is at least one [[Unit]] serving corresponding [[Role]] */
+  get ready() {
+    return this._ready;
+  }
 }
 
 export interface Destination {
-    /**@internal */
-    on(event: string, handler?: (...args: any[]) => void): this
+  /**@internal */
+  on(event: string, handler?: (...args: any[]) => void): this;
 
-    /**Not tested yet. Event 'close' is dispatched when last unit gets disconnected
-     * @event
-     */
-    on(event: 'close', handler: () => void): this
+  /**Not tested yet. Event 'close' is dispatched when last unit gets disconnected
+   * @event
+   */
+  on(event: "close", handler: () => void): this;
 
-    /**Event 'unit' is dispatched when destination gets new unit connected
-     */
-    on(event: 'unit', handler: (unit: Unit) => void): this
+  /**Event 'unit' is dispatched when destination gets new unit connected
+   */
+  on(event: "unit", handler: (unit: Unit) => void): this;
 }
 
-
-function createMessageHeaders(this: Destination, event: string | EmitOptions): MessageHeaders {
-    let headers = typeof event === 'object' && event ? { ...event } : { event } as MessageHeaders;
-    headers.role = this.name;
-    return headers;
+function createMessageHeaders(
+  this: Destination,
+  event: string | EmitOptions
+): MessageHeaders {
+  let headers =
+    typeof event === "object" && event
+      ? { ...event }
+      : ({ event } as MessageHeaders);
+  headers.role = this.name;
+  return headers;
 }
-
-
--- src/Role.ts
+++ src/Role.ts
@@ -1,154 +1,177 @@
-import { EventEmitter } from 'events';
-import { Peer } from './Peer';
-import { ROLES_MESSAGE } from './constants';
-import { Context, ContextForReadable, ContextForWritable, RequestCallback, MessageHandler, RequestHandler, ReadableHandler, WritableHandler } from './interfaces';
+import { EventEmitter } from "events";
+import { Peer } from "./Peer";
+import { ROLES_MESSAGE } from "./constants";
+import {
+  Context,
+  ContextForReadable,
+  ContextForWritable,
+  RequestCallback,
+  MessageHandler,
+  RequestHandler,
+  ReadableHandler,
+  WritableHandler
+} from "./interfaces";
 
 /**Role represents a service on the local Peer. It should handle incoming messages, requests and stream requests for certain functionality
- * 
+ *
  * This class should not be instantiated directly. It is exposed for type declaration and documentation
-*/
+ */
 export class Role extends EventEmitter {
-    /**@internal */
-    _msgHandler: EventEmitter = new EventEmitter();
-    /**@internal */
-    _reqHandler: EventEmitter = new EventEmitter();
-    /**@internal */
-    _readableHandler: EventEmitter = new EventEmitter();
-    /**@internal */
-    _writableHandler: EventEmitter = new EventEmitter();
-    /**@internal */
-    _peer: Peer;
-    /**@internal */
-    _active: boolean;
-    readonly name: string;
-    constructor(name: string, peer: Peer, active?: boolean) {
-        super();
-        if (!name || typeof name !== 'string') {
-            throw new Error(`Role's name should be a string`);
-        }
-        this._peer = peer;
-        this.name = name;
-        if (active === false) {
-            this._active = false;
-        } else {
-            this._active = true;
-        }
+  /**@internal */
+  _msgHandler: EventEmitter = new EventEmitter();
+  /**@internal */
+  _reqHandler: EventEmitter = new EventEmitter();
+  /**@internal */
+  _readableHandler: EventEmitter = new EventEmitter();
+  /**@internal */
+  _writableHandler: EventEmitter = new EventEmitter();
+  /**@internal */
+  _peer: Peer;
+  /**@internal */
+  _active: boolean;
+  readonly name: string;
+  constructor(name: string, peer: Peer, active?: boolean) {
+    super();
+    if (!name || typeof name !== "string") {
+      throw new Error(`Role's name should be a string`);
     }
+    this._peer = peer;
+    this.name = name;
+    if (active === false) {
+      this._active = false;
+    } else {
+      this._active = true;
+    }
+  }
 
-	/**
-     * Set handler for incoming message
-     */
-    onMessage(msg: string | MessageHandler, handler?: MessageHandler) {
-        if (typeof msg === 'string') {
-            return this._msgHandler.on(msg, handler!);
-        }
-        this.on('message', msg);
+  /**
+   * Set handler for incoming message
+   */
+  onMessage(msg: string | MessageHandler, handler?: MessageHandler) {
+    if (typeof msg === "string") {
+      return this._msgHandler.on(msg, handler!);
     }
-	/**
-     * Set handler for incoming request.
-	 */
-    onRequest(msg: string | RequestHandler, handler?: RequestHandler) {
-        if (typeof msg === 'string') {
-            return this._reqHandler.on(msg, handler!);
-        }
-        this.on('request', msg)
+    this.on("message", msg);
+  }
+  /**
+   * Set handler for incoming request.
+   */
+  onRequest(msg: string | RequestHandler, handler?: RequestHandler) {
+    if (typeof msg === "string") {
+      return this._reqHandler.on(msg, handler!);
     }
-	/**
-	* Set handler for incoming request for receiving stream data
-     */
-    onReadable(msg: string, handler: ReadableHandler) {
-        if (typeof msg === 'string') {
-            return this._readableHandler.on(msg, handler);
-        }
-        this.on('readable', msg)
+    this.on("request", msg);
+  }
+  /**
+   * Set handler for incoming request for receiving stream data
+   */
+  onReadable(msg: string, handler: ReadableHandler) {
+    if (typeof msg === "string") {
+      return this._readableHandler.on(msg, handler);
     }
-	/**
-	* Set handler for incoming request for sending stream data
-     */
-    onWritable(msg: string | WritableHandler, handler?: WritableHandler) {
-        if (typeof msg === 'string') {
-            return this._writableHandler.on(msg, handler!)
-        }
-        this.on('writable', msg)
+    this.on("readable", msg);
+  }
+  /**
+   * Set handler for incoming request for sending stream data
+   */
+  onWritable(msg: string | WritableHandler, handler?: WritableHandler) {
+    if (typeof msg === "string") {
+      return this._writableHandler.on(msg, handler!);
     }
-    /**Set handler for any incoming message / request / stream request*/
-    onData(handler: MessageHandler | RequestHandler | WritableHandler | ReadableHandler) {
-        this.on('data', handler)
-    }
-	/**
-	 * Deactivate role. All units will be notified immediately with rebuilding their state
-	 */
-    disable() {
-        this._active = false;
-        this._peer.emit(ROLES_MESSAGE, this);
-    }
-	/**
-	 * Activate role. All units will be notified immediately with rebuilding their state
-	 */
-    enable() {
-        this._active = true;
-        this._peer.emit(ROLES_MESSAGE, this);
-    }
-	/**
-	 * get role status. Active means that role is enabled. If role is not active, all incoming data to the role will be rejected
-	 */
-    get active() {
-        return this._active;
-    }
-    /**@internal */
-    _emitMsg(ctx: Context) {
-        runMiddleware(this, this._msgHandler, 'message', ctx);
-    }
-    /**@internal */
-    _emitReq(ctx: Context, cb: RequestCallback) {
-        runMiddleware(this, this._reqHandler, 'request', ctx, cb);
-    }
-    /**@internal */
-    _emitReadable(ctx: ContextForReadable, cb: RequestCallback) {
-        runMiddleware(this, this._readableHandler, 'readable', ctx, cb);
-    }
-    /**@internal */
-    _emitWritable(ctx: ContextForWritable, cb: RequestCallback) {
-        runMiddleware(this, this._writableHandler, 'writable', ctx, cb);
-    }
+    this.on("writable", msg);
+  }
+  /**Set handler for any incoming message / request / stream request*/
+  onData(
+    handler: MessageHandler | RequestHandler | WritableHandler | ReadableHandler
+  ) {
+    this.on("data", handler);
+  }
+  /**
+   * Deactivate role. All units will be notified immediately with rebuilding their state
+   */
+  disable() {
+    this._active = false;
+    this._peer.emit(ROLES_MESSAGE, this);
+  }
+  /**
+   * Activate role. All units will be notified immediately with rebuilding their state
+   */
+  enable() {
+    this._active = true;
+    this._peer.emit(ROLES_MESSAGE, this);
+  }
+  /**
+   * get role status. Active means that role is enabled. If role is not active, all incoming data to the role will be rejected
+   */
+  get active() {
+    return this._active;
+  }
+
+  /**Get [[Peer]] instance*/
+  get peer() {
+    return this._peer;
+  }
+
+  /**@internal */
+  _emitMsg(ctx: Context) {
+    runMiddleware(this, this._msgHandler, "message", ctx);
+  }
+  /**@internal */
+  _emitReq(ctx: Context, cb: RequestCallback) {
+    runMiddleware(this, this._reqHandler, "request", ctx, cb);
+  }
+  /**@internal */
+  _emitReadable(ctx: ContextForReadable, cb: RequestCallback) {
+    runMiddleware(this, this._readableHandler, "readable", ctx, cb);
+  }
+  /**@internal */
+  _emitWritable(ctx: ContextForWritable, cb: RequestCallback) {
+    runMiddleware(this, this._writableHandler, "writable", ctx, cb);
+  }
 }
 
-function runMiddleware(role: Role, handler: EventEmitter, communicationType: string, ctx: Context | ContextForReadable | ContextForWritable, callback?: RequestCallback) {
-    let cb;
-    let done = false;
-    let resolvers: (() => void)[] = []
-    if (callback) {
-        ctx.next = () => {
-            let bndResolve: (() => void)
-            let prom = new Promise<void>(resolve => {
-                bndResolve = resolve;
-                resolvers.push(() => resolve);
-            })
-            resolvers.push(bndResolve!.bind(prom));
-            return prom
-        };
-        cb = (err: any, res: any) => {
-            done = true;
-            ctx.response = res;
-            ctx.error = err;
-            chainResolvers(resolvers, () => {
-                callback!(ctx.error, ctx.response);
-            });
-        }
-    }
-    role._peer.emit('data', ctx, cb);
-    done || role._peer.emit(communicationType, ctx, cb);
-    done || role.emit('data', ctx, cb);
-    done || role.emit(communicationType, ctx, cb);
-    done || handler.emit(ctx.event!, ctx, cb)
+function runMiddleware(
+  role: Role,
+  handler: EventEmitter,
+  communicationType: string,
+  ctx: Context | ContextForReadable | ContextForWritable,
+  callback?: RequestCallback
+) {
+  let cb;
+  let done = false;
+  let resolvers: (() => void)[] = [];
+  if (callback) {
+    ctx.next = () => {
+      let bndResolve: () => void;
+      let prom = new Promise<void>(resolve => {
+        bndResolve = resolve;
+        resolvers.push(() => resolve);
+      });
+      resolvers.push(bndResolve!.bind(prom));
+      return prom;
+    };
+    cb = (err: any, res: any) => {
+      done = true;
+      ctx.response = res;
+      ctx.error = err;
+      chainResolvers(resolvers, () => {
+        callback!(ctx.error, ctx.response);
+      });
+    };
+  }
+  role._peer.emit("data", ctx, cb);
+  done || role._peer.emit(communicationType, ctx, cb);
+  done || role.emit("data", ctx, cb);
+  done || role.emit(communicationType, ctx, cb);
+  done || handler.emit(ctx.event!, ctx, cb);
 }
 
 function chainResolvers(resolvers: (() => void)[], final: Function) {
-    setImmediate(() => {
-        if (resolvers.length === 0) {
-            return final();
-        }
-        resolvers.pop()!();
-        chainResolvers(resolvers, final);
-    })
+  setImmediate(() => {
+    if (resolvers.length === 0) {
+      return final();
+    }
+    resolvers.pop()!();
+    chainResolvers(resolvers, final);
+  });
 }
